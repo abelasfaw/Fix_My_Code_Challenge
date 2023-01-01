de4b56b4a018f18e7f39d0380dcf964d2eb11d44
abelasfaw <abeloman89@gmail.com>
2023-01-01 04:24:38 -0800

    add comments on each method

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -8,6 +8,7 @@ class square():
     height = 0
 
     def __init__(self, *args, **kwargs):
+        """ square initializtion """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -16,14 +17,16 @@ class square():
         return self.width * self.height
 
     def PermiterOfMySquare(self):
+        """ Permiter of square """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """ string representation of instance """
         return "{}/{}".format(self.width, self.height)
 
 
 if __name__ == "__main__":
-
+    """ create square instance """
     s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
