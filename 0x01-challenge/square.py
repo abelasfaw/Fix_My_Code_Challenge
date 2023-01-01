7d224b5b67418dc9fcdece2359fd7496b4203dd9
abelasfaw <abeloman89@gmail.com>
2023-01-01 04:28:09 -0800

    fix

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -2,7 +2,7 @@
 """ Module for square class"""
 
 
-class square():
+class Square():
     """square class"""
     width = 0
     height = 0
@@ -27,7 +27,7 @@ class square():
 
 if __name__ == "__main__":
     """ create square instance """
-    s = square(width=12, height=9)
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
     print(s.PermiterOfMySquare())
